c9779a6ba08bd4af6cc47d9aadcda753723ffb28
Alexandre Joseph <jexhson@alexandrejoseph.com>
2011-01-06 21:52:46 +0100

    Improve enigma (not successful)

--- src/algo/Enigma.cpp
+++ src/algo/Enigma.cpp
@@ -8,44 +8,66 @@
 #include "Enigma.h"
 
 Enigma::Enigma() {
-    rotors.append("DMTWSILRUYQNKFEJCAZBPGXOHV");
-    rotors.append("HQZGPJTMOBLNCIFDYAWVEUSRKX");
-    rotors.append("UQNTLSZFMREHDPXKIBVYGJCWOA");
-    reflector = "YRUHQSLDPXNGOKMIEBFZCWVJAT";
+    rotors.append("JGDQOXUSCAMIFRVTPNEWKBLZYH");
+    rotors.append("NTZPSFBOKMWRCJDIVLAEYUXHGQ");
+    rotors.append("JVIUBHTCDYAKEQZPOSGXNRMWFL");
+    reflector = "QWERTZUIOASDFGHJKPYXCVBNML";
 }
 
 void Enigma::enigma(bool cipher) {
     QString msg, out = "";
     msg = (cipher) ? clearMsg : cipherMsg;
     
-    int posR1, posR2, curChar, forward, backward;
+    int posR2, posR3, curChar, forward, backward, kbLength = rotors.at(0).length()-1;
     
-    for (curChar=0, posR1=0, posR2=0; curChar < msg.length(); curChar++) {
-        if ((curChar %26) == 0) {
-            if ((posR1 %26) == 0)
-                posR2 = (posR2+1) %26;
+    for (curChar=0, posR2=0, posR3=0; curChar < msg.length(); curChar++) {
+        if (curChar > 0 && (curChar%kbLength) == 0) {
+            if ((posR2%kbLength) == 0)
+                posR3 = (posR3+1)%kbLength;
             
-            posR1 = (posR1+1) %26;
+            posR2 = (posR2+1)%kbLength;
         }
+
+        QString deb = "";
+        deb += msg.at(curChar);
+        int pos = (msg.at(curChar).toAscii()-'A'+curChar+rotor1Pos)%kbLength;
+        deb += QString(" -> %1").arg(rotors.at(0).at(pos));
+        pos = (rotor2Pos+posR2+rotors.at(0).at(pos).toAscii()-'A') % kbLength;
+        deb += QString(" -> %1").arg(rotors.at(1).at(pos));
+        pos = (rotor3Pos+posR3+rotors.at(1).at(pos).toAscii()-'A') % kbLength;
+        deb += QString(" -> %1").arg(rotors.at(2).at(pos));
+        pos = (reflectorPos+rotors.at(2).at(pos).toAscii()-'A') % kbLength;
+        deb += QString(" <-> %1").arg(reflector.at(pos));
+        pos = (rotor3Pos+posR3+reflector.at(pos).toAscii()-'A') % kbLength;
+        deb += QString(" -> %1").arg(rotors.at(2).at(pos));
+        pos = (rotor2Pos+posR2+rotors.at(2).at(pos).toAscii()-'A') % kbLength;
+        deb += QString(" -> %1").arg(rotors.at(1).at(pos));
+        pos = (rotor1Pos+(curChar % kbLength)+rotors.at(1).at(pos).toAscii()-'A') % kbLength;
+        deb += QString(" -> %1").arg(rotors.at(0).at(pos));
+
+        qDebug() << deb;
+
+
 
         forward = reflector.at(
                     (reflectorPos+rotors.at(2).at(
-                        (rotor3Pos+posR2+rotors.at(1).at(
-                            (rotor2Pos+posR1+rotors.at(0).at(
-                                (msg.at(curChar).toAscii()-'A'+curChar+rotor1Pos) %26
-                                                            ).toAscii()) %26
-                                                        ).toAscii()) %26
-                                                  ).toAscii()) %26
-                               ).toAscii()-'A';
-        backward = rotors.at(0).at(
-                        (rotor1Pos+(curChar % 26)+rotors.at(1).at(
-                            (rotor2Pos+posR1+rotors.at(2).at(
-                                (forward+posR2+rotor3Pos) %26
-                                                            ).toAscii()) %26
-                                                       ).toAscii()) %26
-                                   ).toAscii()-'A';
+                        (rotor3Pos+posR3+rotors.at(1).at(
+                            (rotor2Pos+posR2+rotors.at(0).at(
+                                (msg.at(curChar).toAscii()-'A'+curChar+rotor1Pos)%kbLength
+                            ).toAscii()-'A')%kbLength
+                        ).toAscii()-'A')%kbLength
+                    ).toAscii()-'A')%kbLength
+                ).toAscii()-'A';
 
-        out += (QChar)('A'+backward);
+        backward = rotors.at(0).at(
+                    (rotor1Pos+(curChar%kbLength)+rotors.at(1).at(
+                        (rotor2Pos+posR2+rotors.at(2).at(
+                            (forward+posR3+rotor3Pos)%kbLength
+                        ).toAscii()-'A')%kbLength
+                    ).toAscii()-'A')%kbLength
+                ).toAscii();
+
+        out += backward;
     }
 
     if (cipher)
